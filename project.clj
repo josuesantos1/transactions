7ba4efbab756f34dca0a049dbe6ca2c935b2adad
Josué Santos <josue.santos.dev@gmail.com>
2023-01-03 23:11:35 -0300

    :package: add datomic-pro

--- project.clj
+++ project.clj
@@ -5,6 +5,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                   :creds :gpg}}
+
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [io.pedestal/pedestal.service "0.5.11-beta-1"]
 
@@ -17,7 +21,10 @@
                  [ch.qos.logback/logback-classic "1.2.10" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.35"]
                  [org.slf4j/jcl-over-slf4j "1.7.35"]
-                 [org.slf4j/log4j-over-slf4j "1.7.35"]]
+                 [org.slf4j/log4j-over-slf4j "1.7.35"]
+                 
+                 [com.datomic/datomic-pro "1.0.6527"]]
+  
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
